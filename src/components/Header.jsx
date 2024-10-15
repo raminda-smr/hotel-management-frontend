a9457205c985580c2145a5c975bc2836b124d914
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-10-16 02:42:06 +0530

    create user tag for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
+import UserTag from "./UserTag"
+
 function Header(){
     return(
         <header>
             <h1>Hotel Management System</h1>
+            <UserTag/>
         </header>
     )
 }
---
+++ src/components/UserTag.jsx
@@ -0,0 +1,10 @@
+function UserTag(){
+    return(
+        <>
+            <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" style={{width:"50px"}} />
+            <h4>John Doe</h4>
+        </>
+    )
+}
+
+export default UserTag
