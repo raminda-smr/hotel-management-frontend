1171f8af73063e696bb745180e69073f05b7f463
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-10-20 08:47:50 +0530

    link page not found page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { HomePage } from './components/pages/client-pages/homePage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AdminPage } from './components/pages/admin-pages/adminPage'
+import { PageNotFound } from './components/pages/error-pages/pageNotFound'
 
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
             <Routes path="/*">
                 <Route path="/" element={<HomePage />} />
                 <Route path="/admin" element={<AdminPage />} />
+
+                <Route path="/*" element={<PageNotFound />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/components/pages/error-pages/pageNotFound.jsx
@@ -0,0 +1,11 @@
+export function PageNotFound(){
+
+    return (
+        <>
+            <div className="w-full h-screen bg-yellow-100 flex flex-col justify-center items-center">
+                <h1 className="text-[200px] text-gray-600 leading-none">404</h1>
+                <h3 className="text-5xl text-gray-600">Page Not Found</h3>
+            </div>
+        </>
+    )
+}
