16e789ac45ac2afe0074594d6b6da25ed96f102e
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-10-20 14:57:06 +0530

    create basic admin layoute

--- src/components/pages/admin-pages/adminPage.jsx
+++ src/components/pages/admin-pages/adminPage.jsx
@@ -4,39 +4,52 @@ export function AdminPage() {
 
     return (
         <>
-            <div className="w-full h-screen bg-red-100">
-                <div>
-                    <Link className='mr-2' to="/admin/bookings" >Bookings </Link>
-                    <Link className='mr-2' to="/admin/rooms" >Rooms </Link>
-                    <Link className='mr-2' to="/admin/categories" >Categories </Link>
+            <div className="w-full h-screen flex">
+
+                <div className='sidebar w-64 bg-gray-900 p-4'>
+
+                    <div className="sidebar-menu flex flex-col">
+                        <Link className='mb-2 text-white bg-gray-800 px-4 py-2 rounded-lg hover:bg-gray-700' to="/admin/bookings" >Bookings </Link>
+                        <Link className='mb-2 text-white bg-gray-800 px-4 py-2 rounded-lg hover:bg-gray-700' to="/admin/rooms" >Rooms </Link>
+                        <Link className='mb-2 text-white bg-gray-800 px-4 py-2 rounded-lg hover:bg-gray-700' to="/admin/categories" >Categories </Link>
+                    </div>
+
                 </div>
 
-                <Routes path="/*" >
-                    <Route path="/bookings" element={
-                        <div>
-                            <h1>Bookings</h1>
-                        </div>
-                    }></Route>
+                <div className='flex-1 p-4'>
+                    <div className="top-menu"></div>
+                    <div className="content-area">
+                        <Routes path="/*" >
+                            <Route path="/bookings" element={
+                                <div>
+                                    <h1>Bookings</h1>
+                                </div>
+                            }></Route>
 
-                    <Route path="/rooms" element={
-                        <div>
-                            <h1>Rooms</h1>
-                        </div>
-                    }></Route>
+                            <Route path="/rooms" element={
+                                <div>
+                                    <h1>Rooms</h1>
+                                </div>
+                            }></Route>
 
-                    <Route path="/categories" element={
-                        <div>
-                            <h1>Categories</h1>
-                        </div>
-                    }></Route>
+                            <Route path="/categories" element={
+                                <div>
+                                    <h1>Categories</h1>
+                                </div>
+                            }></Route>
 
-                    <Route path="/*" element={
-                        <div>
-                            <h1>404</h1>
-                        </div>
-                    }></Route>
+                            <Route path="/*" element={
+                                <div>
+                                    <h1>404</h1>
+                                </div>
+                            }></Route>
 
-                </Routes>
+                        </Routes>
+                    </div>
+                </div>
+
+
+
 
             </div>
         </>
