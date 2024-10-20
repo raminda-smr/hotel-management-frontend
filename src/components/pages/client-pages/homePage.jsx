03bada7fdb4c2a6e2b2453010f21101819132cb6
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-10-20 13:29:43 +0530

    create about us page

--- src/components/pages/client-pages/homePage.jsx
+++ src/components/pages/client-pages/homePage.jsx
@@ -2,6 +2,7 @@ import IndexPage from "./index/indexPage";
 import Header from "./parts/header";
 import { Routes, Route } from "react-router-dom";
 import SearchRooms from "./search-rooms/searchRooms";
+import AboutUs from "./about-us/aboutUs";
 
 
 function HomePage() {
@@ -13,6 +14,7 @@ function HomePage() {
             <Routes path="/*">
                 <Route path="/" element={<IndexPage />} />
                 <Route path="/search-rooms" element={<SearchRooms />} />
+                <Route path="/about-us" element={<AboutUs />} />
 
             </Routes>
             
