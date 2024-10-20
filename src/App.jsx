a854a8dc5e0d91e656a70a0ed1abaa1b94a3498d
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-10-20 07:30:21 +0530

    design home page

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,26 @@ function App() {
   return (
     <>
       <Header />
-      <div className='w-full h-[100vh] bg-blue-900'></div>
+      <div className='w-full h-screen bg-blue-900 flex flex-col items-center'>
+
+        <div className='border border-white bg-white w-[700px] h-[100px] flex rounded-lg justify-center items-center'>
+
+          <input type="date" name='start-date' />
+
+          <input type="date" name='end-date' />
+
+          <select name="room-category" id="">
+            <option value="Standard">Standard</option>
+            <option value="Luxuary">Luxuary</option>
+            <option value="Supreme">Supreme</option>
+          </select>
+
+          <button>Book Now</button>
+        </div>
+
+        <h1 className='text-white text-5xl'>Welcome to the Lionine Villa</h1>
+
+      </div>
     </>
   )
 }
