ff492dc0d659ebd6283af4cdaa3e6b4880052887
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-10-21 14:13:14 +0530

    update admin dashboard page

--- src/components/pages/admin-pages/dashboard/Dashboard.jsx
+++ src/components/pages/admin-pages/dashboard/Dashboard.jsx
@@ -1,8 +1,18 @@
+import { CiHome } from "react-icons/ci"
+import { TfiAngleRight } from "react-icons/tfi"
+
 function Dashboard(){
 
     return (
         <>
-            <h2 className="text-gray-700 text-3xl">Dashboard</h2>
+            <div className="page-header mb-6 ">
+            <h2 className="text-gray-700 text-2xl mb-2">Dashboard</h2>
+            <div className="path flex items-center text-sm border-b border-gray-300 pb-3 text-gray-500">
+                <span ><CiHome /></span> <TfiAngleRight />
+                <span >Admin</span><TfiAngleRight />
+                <span >Dashboard</span>
+            </div>
+        </div>
         </>
     )
 }
