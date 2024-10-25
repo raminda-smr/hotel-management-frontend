94d2a290c7b40b7be388fbef397e9d026deca2ac
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-10-25 16:47:21 +0530

    install axios and send request

--- src/pages/login-page/LoginPage.jsx
+++ src/pages/login-page/LoginPage.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react"
 import "./LoginPage.css"
+import axios from "axios"
 
 export default function LoginPage() {
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
-    function handleLogin(){
-        console.log('test');
+    function handleLogin() {
+        axios.post("http://localhost:3000/api/users/login", {
+            email: email,
+            password: password
+        }).then(
+            (res)=>{
+                console.log(res)
+            }
+        ).catch(
+            (err)=>{
+                console.log(err)
+            }
+        )
     }
 
     return (
@@ -18,16 +30,16 @@ export default function LoginPage() {
                     <h3 className="text-white text-3xl mt-20 mb-5 ">Login</h3>
 
                     <input type="text" placeholder="Enter your name" className="w-[80%] mb-3 px-4 py-2 border-white border-[2px] rounded-lg bg-transparent text-white " defaultValue={email} onChange={
-                        (e)=>{
+                        (e) => {
                             setEmail(e.target.value)
                         }
-                    }  />
+                    } />
 
                     <input type="password" placeholder="Enter your password" className="w-[80%] mb-3 px-4 py-2 border-white border-[2px] rounded-lg bg-transparent text-white " defaultValue={password} onChange={
-                        (e)=>{
+                        (e) => {
                             setPassword(e.target.value)
                         }
-                    }  />
+                    } />
 
 
                     <button className="w-[80%] bg-red-500 text-white px-4 py-4 mt-10" onClick={handleLogin}>Submit</button>
