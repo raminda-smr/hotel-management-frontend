c42e47d02e801891f39e720f3761549b32eced27
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-10-27 13:17:02 +0530

    fetch user data for user component

--- src/components/client/userdata/userdata.jsx
+++ src/components/client/userdata/userdata.jsx
@@ -1,8 +1,30 @@
-export default function UserTag(){
+import axios from 'axios'
+import { useState } from 'react'
+
+export default function UserTag(props){
+
+    const [name, setName] = useState("")
+
+    const token = localStorage.getItem('token')
+
+    if(token != null){
+        axios.get(import.meta.env.VITE_BACKEND_URL + '/api/users/logged',{
+            headers:{
+                "Authorization" : 'Bearer ' + token,
+                "Content-Type" : "application/json"
+            }
+        })
+        .then(
+            (res)=>{
+                // console.log(res)
+                setName(res.data.user.lastName)
+            }
+        )
+    }
     return(
         <div className="flex items-center cursor-pointer ml-2 bg-gray-100 rounded-lg px-3 py-2">
             <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" className="rounded-full w-[30px] h-[30px]"/>
-            <span className="ml-2 ">Raminda</span>
+            <span className="ml-2 ">{name}</span>
         </div>
     )
 }
