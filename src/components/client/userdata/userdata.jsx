ed090f4185bc0cb1d57d388722a60b8bbe627c00
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-10-27 13:25:30 +0530

    add useeffect hook

--- src/components/client/userdata/userdata.jsx
+++ src/components/client/userdata/userdata.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function UserTag(props){
 
@@ -7,20 +7,23 @@ export default function UserTag(props){
 
     const token = localStorage.getItem('token')
 
-    if(token != null){
-        axios.get(import.meta.env.VITE_BACKEND_URL + '/api/users/logged',{
-            headers:{
-                "Authorization" : 'Bearer ' + token,
-                "Content-Type" : "application/json"
-            }
-        })
-        .then(
-            (res)=>{
-                // console.log(res)
-                setName(res.data.user.lastName)
-            }
-        )
-    }
+    useEffect(()=>{
+        if(token != null){
+            axios.get(import.meta.env.VITE_BACKEND_URL + '/api/users/logged',{
+                headers:{
+                    "Authorization" : 'Bearer ' + token,
+                    "Content-Type" : "application/json"
+                }
+            })
+            .then(
+                (res)=>{
+                    console.log(res)
+                    setName(res.data.user.lastName)
+                }
+            )
+        }
+    },[])
+    
     return(
         <div className="flex items-center cursor-pointer ml-2 bg-gray-100 rounded-lg px-3 py-2">
             <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" className="rounded-full w-[30px] h-[30px]"/>
