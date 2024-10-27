5cbc2f9ad8eb9099d089d12ccfbdd22890252de0
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-10-27 14:54:33 +0530

    move admin category page

--- src/components/client/userdata/userdata.jsx
+++ src/components/client/userdata/userdata.jsx
@@ -4,10 +4,12 @@ import { useEffect, useState } from 'react'
 export default function UserTag(props){
 
     const [name, setName] = useState("")
-
-    const token = localStorage.getItem('token')
+    const [userFound, setUserFound] = useState(false)
 
     useEffect(()=>{
+        
+        const token = localStorage.getItem('token')
+
         if(token != null){
             axios.get(import.meta.env.VITE_BACKEND_URL + '/api/users/logged',{
                 headers:{
@@ -19,15 +21,24 @@ export default function UserTag(props){
                 (res)=>{
                     console.log(res)
                     setName(res.data.user.lastName)
+                    setUserFound(true)
                 }
             )
         }
-    },[])
+        else{
+            setName("")
+        }
+    },[userFound])
     
     return(
         <div className="flex items-center cursor-pointer ml-2 bg-gray-100 rounded-lg px-3 py-2">
             <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" className="rounded-full w-[30px] h-[30px]"/>
             <span className="ml-2 ">{name}</span>
+
+            <button onClick={()=>{
+                localStorage.removeItem('token')
+                setUserFound(false)
+            }}></button>
         </div>
     )
 }
