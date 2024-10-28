65766a010494b444419e120c00c7646e574ad796
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-10-28 22:41:01 +0530

    move gallery page and load live data

---
+++ src/pages/admin-pages/gallery.jsx
@@ -0,0 +1,99 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import PageHeader from "../../components/admin/page-header/pageHeader"
+import AdminTable from "../../components/admin/admin-table/adminTable"
+import AdminTableHead from "../../components/admin/admin-table/adminTableHead"
+import AdminTableRow from "../../components/admin/admin-table/adminTableRow"
+import AdminTableTH from "../../components/admin/admin-table/adminTableTH"
+import AdminTableBody from "../../components/admin/admin-table/adminTableBody"
+import AdminTableTD from "../../components/admin/admin-table/adminTableTD"
+
+
+
+export default function Gallery() {
+
+    const [galleryItems, setGalleryItems] = useState([])
+    const [isGalleryItemsLoaded, setIsGalleryItemsLoaded] = useState(false)
+
+    const tableFields = ['Image', 'Name', 'Description', 'Actions']
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+
+        if (token != null && !isGalleryItemsLoaded) {
+            axios.get(import.meta.env.VITE_BACKEND_URL + '/api/gallary', {}).then(
+                (res) => {
+                    // console.log(cats)
+                    setGalleryItems(res.data.list)
+                    setIsGalleryItemsLoaded(true)
+                }
+            )
+        }
+
+    }, [isGalleryItemsLoaded])
+
+
+    function deleteItem(id) {
+        const token = localStorage.getItem('token')
+
+        if (token != null) {
+            axios.delete(import.meta.env.VITE_BACKEND_URL + '/api/gallery/' + id, {
+                headers: {
+                    "Authorization": 'Bearer ' + token,
+                    "Content-Type": "application/json"
+                }
+            }).then(
+                (res) => {
+                    setIsGalleryItemsLoaded(false)
+                }
+            )
+        }
+    }
+
+
+    return (
+        <>
+            <PageHeader to="/admin/gallery" name="Gallery" title="Gallery" />
+
+
+            <div className="gallery-data">
+            
+                <AdminTable>
+                    <AdminTableHead className="rounded-lg overflow-hidden">
+                        <AdminTableRow className="bg-blue-500">
+                            {
+                                tableFields.map(
+                                    (tableField, index) => <AdminTableTH key={index}>{tableField}</AdminTableTH>
+                                )
+                            }
+                        </AdminTableRow>
+
+                    </AdminTableHead>
+                    <AdminTableBody>
+                        {
+                            galleryItems.map(
+                                (galleryItem, index) => {
+                                    return (
+                                        <AdminTableRow key={index}>
+                                            <AdminTableTD>
+                                                <img src={galleryItem.image} className="w-[100px]" alt="" />
+                                            </AdminTableTD>
+                                            <AdminTableTD>{galleryItem.name}</AdminTableTD>
+                                            <AdminTableTD>{galleryItem.description.substring(0,50)}</AdminTableTD>
+                                          
+                                            <AdminTableTD>
+                                                <button className="bg-red-400 text-white text-xs px-2 py-1 rounded-md" onClick={() => { deleteItem(galleryItem._id) }}>Delete</button>
+                                            </AdminTableTD>
+                                        </AdminTableRow>
+                                    )
+                                }
+                            )
+                        }
+
+                    </AdminTableBody>
+                </AdminTable>
+            </div>
+
+        </>
+    )
+}
