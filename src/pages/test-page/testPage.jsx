b2fc357da9186b94e5d581f500f9fa54c414e8bd
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-10-29 07:34:27 +0530

    upload file to firebase

--- src/pages/test-page/testPage.jsx
+++ src/pages/test-page/testPage.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react"
+import uploadMedia from "../../utils/mediaUpload"
+
 export default function TestPage() {
-  return (
-    <div>test</div>
-  )
+    const [file, setFile] =useState(null)
+
+    return (
+        <div>
+            <input type="file" defaultChecked={file} onChange={(e) => {
+                setFile(e.target.files[0])
+            }} />
+
+
+            <button onClick={()=>{
+                uploadMedia(file)
+            }}>Submit</button>
+        </div>
+    )
 }
