eb2802f2b26800efdb04b4cfcf487b1b10bb5cb2
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-09 04:38:40 +0530

    dashboard module stat created

--- src/pages/admin-pages/dashboard/dashboard.jsx
+++ src/pages/admin-pages/dashboard/dashboard.jsx
@@ -1,7 +1,8 @@
-import { CiBookmarkCheck, CiCalendar, CiCalendarDate } from "react-icons/ci"
+import { CiBookmarkCheck, CiCalendar, CiCalendarDate, CiChat2, CiHome, CiImageOn, CiShirt, CiUser, CiViewTable } from "react-icons/ci"
 import PageHeader from "../../../components/admin/page-header/pageHeader"
 import { PiClockClockwise } from "react-icons/pi"
 import BookingItem from "./bookingItem"
+import ModuleStat from "./moduleStats"
 
 
 function Dashboard() {
@@ -13,13 +14,19 @@ function Dashboard() {
             <div className="dashboard-data p-4">
 
 
-                <div className="flex">
-                    .item
+                <div className="flex mb-4 gap-5">
+                    <ModuleStat to="/admin/bookings" icon={<CiBookmarkCheck />} title="Bookings" count="300" className="bg-amber-700" />
+                    <ModuleStat to="/admin/categories" icon={<CiViewTable />} title="Categories" count="300" className="bg-teal-600" />
+                    <ModuleStat to="/admin/rooms" icon={<CiHome />} title="Rooms" count="300" className="bg-green-600" />
+                    <ModuleStat to="/admin/users" icon={<CiUser />} title="Customers" count="300" className="bg-cyan-600" />
+                    <ModuleStat to="/admin/users" icon={<CiShirt />} title="Admins" count="300" className="bg-violet-500" />
+                    <ModuleStat to="/admin/feedbacks" icon={<CiChat2 />} title="Feedbacks" count="300" className="bg-pink-500" />
+                    <ModuleStat to="/admin/gallery" icon={<CiImageOn />} title="Gallery Items" count="300" className="bg-rose-500" />
                 </div>
 
-                <div className="flex">
+                <div className="flex gap-5">
 
-                    <div className="bg-amber-500 w-full m-3 p-4 rounded">
+                    <div className="bg-amber-600 w-full p-4 rounded">
                         <h2 className="text-5xl text-white flex items-center border-b border-white pb-2 mb-4">
                             <CiBookmarkCheck /> 
                             <span className="text-2xl ml-1">Bookings</span> 
@@ -27,21 +34,21 @@ function Dashboard() {
 
                         
 
-                        <p className="bg-amber-950 text-white p-1 px-3 mb-1 rounded-md"> Today's Bookings
+                        <p className="bg-amber-400  p-1 px-3 mb-1 rounded-md"> Today's Bookings
                             <span className="bg-amber-400 px-2 py-0 ml-2 rounded-sm text-black">2</span>
                         </p>
-                        <p className="bg-amber-950 text-white p-1 px-3 mb-1 rounded-md"> Upcoming Bookings
+                        <p className="bg-amber-400  p-1 px-3 mb-1 rounded-md"> Upcoming Bookings
                             <span className="bg-amber-300 px-2 py-0 ml-2 rounded-sm text-black">30</span>
                         </p>
-                        <p className="bg-amber-950 text-white p-1 px-3 mb-1 rounded-md"> Completed Bookings
+                        <p className="bg-amber-400  p-1 px-3 mb-1 rounded-md"> Completed Bookings
                             <span className="bg-green-300 px-2 py-0 ml-2 rounded-sm text-black">100</span>
                         </p>
-                        <p className="bg-amber-950 text-white p-1 px-3 mb-1 rounded-md"> Total Bookings
+                        <p className="bg-amber-400  p-1 px-3 mb-1 rounded-md"> Total Bookings
                             <span className="bg-blue-300 px-2 py-0 ml-2 rounded-sm text-black">310</span>
                         </p>
                     </div>
 
-                    <div className="bg-amber-600 w-full m-3 p-4 rounded">
+                    <div className="bg-amber-700 w-full  p-4 rounded">
                         <h2 className="text-5xl text-white flex items-center border-b border-white pb-2 mb-4">
                             <CiCalendarDate /> <span className="text-2xl  ml-1">Today's Bookings</span>
                         </h2>
@@ -52,7 +59,7 @@ function Dashboard() {
                         
                     </div>
 
-                    <div className="bg-amber-700 w-full m-3 p-4 rounded">
+                    <div className="bg-amber-800 w-full p-4 rounded">
                         <h2 className="text-5xl text-white flex items-center border-b border-white pb-2 mb-4">
                             <PiClockClockwise /> <span className="text-2xl  ml-1">Tomorrow's Bookings</span>
                         </h2>
