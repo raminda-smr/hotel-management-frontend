c996e8c7ba4797e092b1c55251f438f3e6dd9c20
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-09 03:55:59 +0530

    dashboard booking items created

---
+++ src/pages/admin-pages/dashboard/bookingItem.jsx
@@ -0,0 +1,30 @@
+export default function BookingItem(props) {
+    return (
+        <div className="booking bg-white   p-1 mb-2 rounded-md flex flex-col">
+            <div className="data-row flex justify-between text-white">
+                <div className="booking-data flex items-stretch text-sm">
+                    <div className="icon bg-blue-500 px-3 py-1 rounded-l-md">Booking</div>
+                    <div className="value bg-blue-600 px-3 py-1 rounded-r-md">1201</div>
+                </div>
+
+                <div className="booking-data flex items-stretch text-sm">
+                    <div className="icon bg-amber-500 px-3 py-1 rounded-l-md">Room</div>
+                    <div className="value bg-amber-600 px-3 py-1 rounded-r-md">106</div>
+                </div>
+            </div>
+
+            <table className="min-w-full bg-white border border-gray-300 shadow-lg text-sm mt-1">
+                <tbody>
+                    <tr className="border-b">
+                        <td className="px-2 py-1 text-left font-semibold text-gray-600 bg-gray-100">Email</td>
+                        <td className="px-2 py-1 text-gray-800">ramindalaksiri91@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td className="px-2 py-1 text-left font-semibold text-gray-600 bg-gray-100">Phone</td>
+                        <td className="px-2 py-1 text-gray-800">0717414210</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
--- src/pages/admin-pages/dashboard/dashboard.jsx
+++ src/pages/admin-pages/dashboard/dashboard.jsx
@@ -1,5 +1,7 @@
-import { CiBookmarkCheck } from "react-icons/ci"
+import { CiBookmarkCheck, CiCalendar, CiCalendarDate } from "react-icons/ci"
 import PageHeader from "../../../components/admin/page-header/pageHeader"
+import { PiClockClockwise } from "react-icons/pi"
+import BookingItem from "./bookingItem"
 
 
 function Dashboard() {
@@ -10,17 +12,21 @@ function Dashboard() {
 
             <div className="dashboard-data p-4">
 
+
                 <div className="flex">
-                    
+                    .item
+                </div>
+
+                <div className="flex">
+
                     <div className="bg-amber-500 w-full m-3 p-4 rounded">
                         <h2 className="text-5xl text-white flex items-center border-b border-white pb-2 mb-4">
-                            <CiBookmarkCheck /> <span className="text-2xl">Bookings</span>
+                            <CiBookmarkCheck /> 
+                            <span className="text-2xl ml-1">Bookings</span> 
                         </h2>
 
-                        <p className=" text-white p-1 px-3 mb-1 rounded-md border-dashed border-white border"> Today's Bookings
-                            <span className="bg-amber-400 px-2 py-0 ml-2 rounded-sm text-black">2</span>
-                        </p>
                         
+
                         <p className="bg-amber-950 text-white p-1 px-3 mb-1 rounded-md"> Today's Bookings
                             <span className="bg-amber-400 px-2 py-0 ml-2 rounded-sm text-black">2</span>
                         </p>
@@ -30,13 +36,31 @@ function Dashboard() {
                         <p className="bg-amber-950 text-white p-1 px-3 mb-1 rounded-md"> Completed Bookings
                             <span className="bg-green-300 px-2 py-0 ml-2 rounded-sm text-black">100</span>
                         </p>
+                        <p className="bg-amber-950 text-white p-1 px-3 mb-1 rounded-md"> Total Bookings
+                            <span className="bg-blue-300 px-2 py-0 ml-2 rounded-sm text-black">310</span>
+                        </p>
                     </div>
 
-                    <div className="bg-white w-full m-3  p-4">
-                        test
+                    <div className="bg-amber-600 w-full m-3 p-4 rounded">
+                        <h2 className="text-5xl text-white flex items-center border-b border-white pb-2 mb-4">
+                            <CiCalendarDate /> <span className="text-2xl  ml-1">Today's Bookings</span>
+                        </h2>
+
+                        <BookingItem />
+                        <BookingItem />
+
+                        
                     </div>
-                    <div className="bg-white w-full m-3  p-4">
-                        test
+
+                    <div className="bg-amber-700 w-full m-3 p-4 rounded">
+                        <h2 className="text-5xl text-white flex items-center border-b border-white pb-2 mb-4">
+                            <PiClockClockwise /> <span className="text-2xl  ml-1">Tomorrow's Bookings</span>
+                        </h2>
+
+                        <BookingItem />
+
+                        <BookingItem />
+
                     </div>
                 </div>
             </div>
