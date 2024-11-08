053b05a38299b386573f324a3f0779b124bf7ab6
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-08 17:26:35 +0530

    add card to dashboard

--- src/pages/admin-pages/dashboard/dashboard.jsx
+++ src/pages/admin-pages/dashboard/dashboard.jsx
@@ -8,7 +8,38 @@ function Dashboard() {
         <>
             <PageHeader to="/admin" name="Dashboard" title="Dashboard" />
 
-            
+            <div className="dashboard-data p-4">
+
+                <div className="flex">
+                    
+                    <div className="bg-amber-500 w-full m-3 p-4 rounded">
+                        <h2 className="text-5xl text-white flex items-center border-b border-white pb-2 mb-4">
+                            <CiBookmarkCheck /> <span className="text-2xl">Bookings</span>
+                        </h2>
+
+                        <p className=" text-white p-1 px-3 mb-1 rounded-md border-dashed border-white border"> Today's Bookings
+                            <span className="bg-amber-400 px-2 py-0 ml-2 rounded-sm text-black">2</span>
+                        </p>
+                        
+                        <p className="bg-amber-950 text-white p-1 px-3 mb-1 rounded-md"> Today's Bookings
+                            <span className="bg-amber-400 px-2 py-0 ml-2 rounded-sm text-black">2</span>
+                        </p>
+                        <p className="bg-amber-950 text-white p-1 px-3 mb-1 rounded-md"> Upcoming Bookings
+                            <span className="bg-amber-300 px-2 py-0 ml-2 rounded-sm text-black">30</span>
+                        </p>
+                        <p className="bg-amber-950 text-white p-1 px-3 mb-1 rounded-md"> Completed Bookings
+                            <span className="bg-green-300 px-2 py-0 ml-2 rounded-sm text-black">100</span>
+                        </p>
+                    </div>
+
+                    <div className="bg-white w-full m-3  p-4">
+                        test
+                    </div>
+                    <div className="bg-white w-full m-3  p-4">
+                        test
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
