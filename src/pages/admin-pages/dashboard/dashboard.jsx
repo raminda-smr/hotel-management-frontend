a6f6fe1336ea0d9f297b952441729a446bf6c767
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-08 17:26:03 +0530

    move dashboard to pages

---
+++ src/pages/admin-pages/dashboard/dashboard.jsx
@@ -0,0 +1,16 @@
+import { CiBookmarkCheck } from "react-icons/ci"
+import PageHeader from "../../../components/admin/page-header/pageHeader"
+
+
+function Dashboard() {
+
+    return (
+        <>
+            <PageHeader to="/admin" name="Dashboard" title="Dashboard" />
+
+            
+        </>
+    )
+}
+
+export default Dashboard
