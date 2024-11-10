dc50d6937cf2ff55d2e0b7ae669b38ee66a193d1
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-10 18:21:03 +0530

    change nav link hover color

--- src/components/client/header/navigation-menu/navigationLink.jsx
+++ src/components/client/header/navigation-menu/navigationLink.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function NavigationLink(props) {
     return (
-        <Link onClick={props.onClick} className="px-6 py-2 select-none hover:bg-gray-200 flex items-center lg:py-5 lg:px-3 lg:hover:bg-amber-500  transition-all" to={props.to}>
+        <Link onClick={props.onClick} className="px-6 py-2 select-none hover:bg-gray-200 flex items-center lg:py-5 lg:px-3 lg:hover:bg-[#2e6fd9] transition-all" to={props.to}>
             <span className="text-xl">{props.icon}</span>
             <span className="pl-2 lg:pl-1">{props.children}</span>
         </Link>
