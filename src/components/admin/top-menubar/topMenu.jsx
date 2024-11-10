c4e2ddbd2c42116fc64365c04e195c1ebb2e7f75
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-10 07:06:22 +0530

    fixed dashboard issue

--- src/components/admin/top-menubar/topMenu.jsx
+++ src/components/admin/top-menubar/topMenu.jsx
@@ -1,18 +1,16 @@
 function TopMenu(){
 
 return(
-    <div className="top-menu w-full bg-gray-800 flex justify-between items-center px-4 py-2">
+    <div className="top-menu w-full bg-gray-800 flex justify-between items-center">
         <div className="div">
-            <h4 className="text-white">Welcome to Lionine Villa Backend!</h4>
+            <h4 className="text-white pl-4">Welcome to Lionine Villa Backend!</h4>
         </div>
 
-        <div className="user-data flex items-center bg-slate-50 rounded-lg px-3 py-2">
+        <div className="user-data flex items-center bg-slate-900 -lg px-3 py-2">
             <div className="user-image">
                  <img className="w-[30px] h-[30px]" src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" />
             </div>
-            <div className="user-name ml-2">
-                Raminda
-            </div>
+            <div className="user-name ml-2">Raminda</div>
         </div>
     </div>
 )
