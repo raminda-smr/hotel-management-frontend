7f844781d7249eb83f9d6e83d1da4762b045b014
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-10 10:29:35 +0530

    created user dropdown

--- src/components/admin/top-menubar/topMenu.jsx
+++ src/components/admin/top-menubar/topMenu.jsx
@@ -1,3 +1,5 @@
+import UserDropdown from "./userDropdown"
+
 function TopMenu(){
 
 return(
@@ -6,12 +8,8 @@ return(
             <h4 className="text-white pl-4">Welcome to Lionine Villa Backend!</h4>
         </div>
 
-        <div className="user-data flex items-center bg-slate-900 -lg px-3 py-2">
-            <div className="user-image">
-                 <img className="w-[30px] h-[30px]" src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" />
-            </div>
-            <div className="user-name ml-2">Raminda</div>
-        </div>
+        <UserDropdown />
+        
     </div>
 )
 
