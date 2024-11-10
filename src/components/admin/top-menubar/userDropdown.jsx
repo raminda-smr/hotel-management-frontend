d1cab5f7cddfbf29fe52f9605d9cb482c957d740
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-10 10:31:57 +0530

    create user dropdown button

--- src/components/admin/top-menubar/userDropdown.jsx
+++ src/components/admin/top-menubar/userDropdown.jsx
@@ -1,15 +1,10 @@
+import UserDropdownButton from "./userDropdownButton";
 
 export default function UserDropdown() {
     return (
-        <div className="user-data flex items-center bg-slate-900 -lg px-3 py-2">
-            <div className="user-image">
-                <img className="w-[30px] h-[30px]" src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" />
-            </div>
-            <div className="user-name ml-2 text-white flex flex-col">
-                <span>Raminda Laksiri</span>
-                <span className="text-xs">ramindalaksiri91@gmail.com</span>
-            </div>
-        </div>
+        <>
+            <UserDropdownButton />
+        </>
 
     )
 }
