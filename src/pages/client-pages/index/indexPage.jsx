c95057de5d53dbe4c0577a9cdb5fece62b0e29c6
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-12 02:37:22 +0530

    design welcome section

--- src/pages/client-pages/index/indexPage.jsx
+++ src/pages/client-pages/index/indexPage.jsx
@@ -3,14 +3,14 @@ import ImageSlider from "../../../components/client/image-slider/imageSlider";
 export default function IndexPage() {
 
     const slides = [
-        { "url":"http://localhost:5173/slides/slide-1.jpg", "title": "Welcome Lionion Vila Resort", "subtitle": "Beach Resort" },
-        { "url":"http://localhost:5173/slides/slide-2.jpg", "title": "Beach", "subtitle": "Beach Resort" },
-        { "url":"http://localhost:5173/slides/slide-3.jpg", "title": "Beach", "subtitle": "Beach Resort" },
-        { "url":"http://localhost:5173/slides/slide-4.jpg", "title": "Beach", "subtitle": "Beach Resort" },
-        { "url":"http://localhost:5173/slides/slide-5.jpg", "title": "Beach", "subtitle": "Beach Resort" }
+        { "url": "http://localhost:5173/slides/slide-1.jpg", "title": "Welcome Lionion Vila Resort", "subtitle": "Beach Resort" },
+        { "url": "http://localhost:5173/slides/slide-2.jpg", "title": "Beach", "subtitle": "Beach Resort" },
+        { "url": "http://localhost:5173/slides/slide-3.jpg", "title": "Beach", "subtitle": "Beach Resort" },
+        { "url": "http://localhost:5173/slides/slide-4.jpg", "title": "Beach", "subtitle": "Beach Resort" },
+        { "url": "http://localhost:5173/slides/slide-5.jpg", "title": "Beach", "subtitle": "Beach Resort" }
     ]
 
-    const settings= {
+    const settings = {
         autoSlide: true,
         slideDuration: 5000,  // in ms
         dots: false,
@@ -23,28 +23,37 @@ export default function IndexPage() {
 
     return (
         <>
-            <ImageSlider slides={slides} settings={settings} />
+            <section className="search-rooms relative">
+                <ImageSlider slides={slides} settings={settings} />
 
-            <div className='w-full h-screen bg-blue-900 flex flex-col items-center'>
+                <div className='absolute top-0 w-full h-full flex justify-center items-center z-20'>
+                    <div className='bg-blue-500 flex flex-col rounded-lg justify-center items-center p-4  min-w-[260px]'>
+                        <div className="flex flex-col w-full md:flex-row">
+                            <input type="date" name='start_date' className="w-full rounded py-2 px-4 mb-2 mr-2" />
+                            <input type="date" name='end_date' className="w-full rounded py-2 px-4 mb-2" />
+                        </div>
+                        <select name="room-category" id="" className="w-full rounded py-2 px-4 mb-2">
+                            <option value="Standard">Standard</option>
+                            <option value="Luxuary">Luxuary</option>
+                            <option value="Supreme">Supreme</option>
+                        </select>
+                        <button className="bg-amber-400 px-6 py-2 rounded-lg  transition-all duration-300 overflow-hidden outline-amber-400 hover:bg-yellow-600 hover:outline-white hover:outline hover:outline-2 hover:-outline-offset-4 hover:text-white">Book Now</button>
+                    </div>
 
-                <div className='border border-white bg-white w-[700px] h-[100px] flex rounded-lg justify-center items-center'>
 
-                    <input type="date" name='start-date' />
 
-                    <input type="date" name='end-date' />
-
-                    <select name="room-category" id="">
-                        <option value="Standard">Standard</option>
-                        <option value="Luxuary">Luxuary</option>
-                        <option value="Supreme">Supreme</option>
-                    </select>
-
-                    <button>Book Now</button>
                 </div>
+            </section>
 
-                <h1 className='text-white text-5xl'>Welcome to the Lionine Villa</h1>
+            <section className="welcome py-20 bg-gray-100">
+                <div className="max-w-[768px] mx-auto px-4">
+                    <h1 className='text-gray-500 text-5xl text-center mb-3'>Welcome to the Leonine Villa</h1>
+                    <p className="text-center text-gray-500 text-xl font-thin ">Nestled along the scenic shores of Negombo, Gampaha in Sri Lanka, Leonine Villa offers the ultimate beachside escape. Our villa is a sanctuary of relaxation and luxury, with every detail crafted to provide an unforgettable experience.</p>
+                </div>
+            </section>
 
-            </div>
+            
+
         </>
 
     )
