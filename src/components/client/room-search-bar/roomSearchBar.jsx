6c14a4987e644978628f7e0e04fd749b7b2c341a
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-12 09:05:49 +0530

    create and link room search bar

---
+++ src/components/client/room-search-bar/roomSearchBar.jsx
@@ -0,0 +1,18 @@
+
+
+export default function RoomSearchbar() {
+    return (
+        <div className='bg-blue-500 flex flex-col rounded-lg justify-center items-center p-4  min-w-[260px]'>
+            <div className="flex flex-col w-full md:flex-row">
+                <input type="date" name='start_date' className="w-full rounded py-2 px-4 mb-2 mr-2" />
+                <input type="date" name='end_date' className="w-full rounded py-2 px-4 mb-2" />
+            </div>
+            <select name="room-category" id="" className="w-full rounded py-2 px-4 mb-2">
+                <option value="Standard">Standard</option>
+                <option value="Luxuary">Luxuary</option>
+                <option value="Supreme">Supreme</option>
+            </select>
+            <button className="bg-amber-400 px-6 py-2 rounded-lg  transition-all duration-300 overflow-hidden outline-amber-400 hover:bg-yellow-600 hover:outline-white hover:outline hover:outline-2 hover:-outline-offset-4 hover:text-white">Book Now</button>
+        </div>
+    )
+}
--- src/pages/client-pages/index/indexPage.jsx
+++ src/pages/client-pages/index/indexPage.jsx
@@ -3,6 +3,7 @@ import ImageSlider from "../../../components/client/image-slider/imageSlider";
 import { CiZoomIn } from "react-icons/ci";
 import GalleryItem from "../../../components/client/gallery-item/galleryItem";
 import FeedbackItem from "../../../components/client/feedback-item/feedbackItem";
+import RoomSearchbar from "../../../components/client/room-search-bar/roomSearchbar";
 
 export default function IndexPage() {
 
@@ -47,20 +48,7 @@ export default function IndexPage() {
                 <ImageSlider slides={slides} settings={settings} />
 
                 <div className='absolute top-0 w-full h-full flex justify-center items-center z-20'>
-                    <div className='bg-blue-500 flex flex-col rounded-lg justify-center items-center p-4  min-w-[260px]'>
-                        <div className="flex flex-col w-full md:flex-row">
-                            <input type="date" name='start_date' className="w-full rounded py-2 px-4 mb-2 mr-2" />
-                            <input type="date" name='end_date' className="w-full rounded py-2 px-4 mb-2" />
-                        </div>
-                        <select name="room-category" id="" className="w-full rounded py-2 px-4 mb-2">
-                            <option value="Standard">Standard</option>
-                            <option value="Luxuary">Luxuary</option>
-                            <option value="Supreme">Supreme</option>
-                        </select>
-                        <button className="bg-amber-400 px-6 py-2 rounded-lg  transition-all duration-300 overflow-hidden outline-amber-400 hover:bg-yellow-600 hover:outline-white hover:outline hover:outline-2 hover:-outline-offset-4 hover:text-white">Book Now</button>
-                    </div>
-
-
+                    <RoomSearchbar />
 
                 </div>
             </section>
