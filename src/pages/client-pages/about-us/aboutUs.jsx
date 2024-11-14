52520419a698d767da27f5b6e6f23a4fea63feab
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-14 18:17:22 +0530

    removed old design

--- src/pages/client-pages/about-us/aboutUs.jsx
+++ src/pages/client-pages/about-us/aboutUs.jsx
@@ -51,7 +51,7 @@ export default function AboutUs() {
 
             {/* Room Structure */}
 
-            <section className="structure py-20">
+            <section className="structure bg-gray-800 py-20">
                 <h2 className="text-6xl text-gray-500 mb-4 text-center" >Room Structure</h2>
                 <p className="text-center text-gray-500 text-xl font-thin ">Our rooms cater to every need, offering three categories for your comfort</p>
 
@@ -146,42 +146,6 @@ export default function AboutUs() {
             </section>
 
 
-            <div className='w-full h-screen bg-sky-600'>
-                <h1 className="text-white text-5xl text-center pt-10 pb-10">About us</h1>
-
-                <div className="container w-[1200px] mx-auto">
-
-                    <div className="grid grid-cols-2 gap-4">
-                        <div className="about-image">
-                            <img src="https://www.thedivinevilla.com/upload/room/gallery/-360145419_190842995487_1674122999_n.jpg" alt="" className='w-full rounded-lg' />
-                        </div>
-                        <div className="content">
-                            <h2 className='text-white text-3xl mb-5 uppercase'>Lionine Villa</h2>
-                            <p className='text-gray-100 text-lg'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat sint ipsam dolorem perferendis? Modi hic, autem cupiditate natus reprehenderit facilis beatae error accusantium dignissimos, sequi nam illum delectus iusto aut? Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe veritatis expedita blanditiis beatae autem facilis quisquam vero, eius quaerat repudiandae, quae reiciendis iure! Provident accusamus autem perspiciatis molestias, eum iusto. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam, quis corporis. Inventore commodi, possimus alias a et maxime. Magnam obcaecati fuga nemo consectetur iusto aliquam mollitia iste eaque laboriosam perferendis.</p>
-                        </div>
-
-                    </div>
-
-
-
-                    <div className="grid grid-cols-4 gap-4 mt-5">
-                        <div>
-                            <img src="https://www.thedivinevilla.com/upload/room/gallery/-82877919081_108325221825_1676617647_n.jpg" alt="" className='rounded-lg' />
-                        </div>
-                        <div>
-                            <img src="https://www.thedivinevilla.com/upload/room/gallery/-313142954_190889997952_1674122913_n.jpg" alt="" className='rounded-lg' />
-                        </div>
-                        <div>
-                            <img src="https://www.thedivinevilla.com/upload/room/gallery/-359137559_190844003347_1674122912_n.jpg" alt="" className='rounded-lg' />
-                        </div>
-                        <div>
-                            <img src="https://www.thedivinevilla.com/upload/room/gallery/-108099078_191095041828_1674122911_n.jpg" alt="" className='rounded-lg' />
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
         </>
     )
 }
