f7a36cbb5859b5e48b162f62dd50e7a4f96a244f
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-15 19:57:11 +0530

    add contact info

--- src/pages/client-pages/contact-us/contactUs.jsx
+++ src/pages/client-pages/contact-us/contactUs.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import PageWrapper from '../../../components/client/page-wrapper/pageWrapper'
+import { CiLocationOn, CiMail, CiPhone } from 'react-icons/ci'
 
 function ContactUs() {
 
@@ -7,44 +8,52 @@ function ContactUs() {
         <>
             <PageWrapper title="Contact us" image="/images/wrappers/about-us.jpg" />
 
-            <section className='w-full h-screen'>
+            <section className='contact-us w-full bg-gray-50 py-10'>
 
                 <h3 className="text-gray-500 text-2xl text-center pt-10">Get In Touch</h3>
                 <h1 className="text-gray-500 text-5xl text-center pb-10">Our Contact Information</h1>
 
-                <div className="container w-[1200px] mx-auto bg-gray-200 rounded-lg">
-                    <div className="grid grid-cols-2 gap-4">
-                        <div className='p-4'>
-                            <div className="contact-item flex items-center bg-white rounded-lg p-4 mb-5">
-                                <div className="image">
-                                    <img className='w-[50px]' src="https://cdn-icons-png.freepik.com/256/535/535239.png?semt=ais_hybrid" alt="" />
-                                </div>
-                                <div className="content ml-3">
-                                    <p>Bokale, pitakoratuwa, Heenatigala</p>
-                                </div>
-                            </div>
+                <div className="max-w-[1200px] grid grid-cols-1 gap-4 px-4 mx-auto md:grid-cols-2 lg:grid-cols-3 ">
 
-                            <div className="contact-item flex items-center bg-white rounded-lg p-4 mb-5">
-                                <div className="image">
-                                    <img className='w-[50px]' src="https://img.freepik.com/premium-vector/phone-call-icon-vector-illustration-design-telephone_373520-4523.jpg" alt="" />
-                                </div>
-                                <div className="content ml-3">
-                                    <p>+94 77 761 2199</p>
-                                </div>
-                            </div>
-
-                            <div className="contact-item flex items-center bg-white rounded-lg p-4 mb-5">
-                                <div className="image">
-                                    <img className='w-[50px]' src="https://cdn-icons-png.freepik.com/256/561/561127.png?semt=ais_hybrid" alt="" />
-                                </div>
-                                <div className="content ml-3">
-                                    <p>thedivinevillaslk@gmail.com</p>
-                                </div>
-                            </div>
-
-
-
+                    <div className="contact-item flex items-center bg-white rounded-lg p-4 mb-5 border border-gray-300 group transition-all">
+                        <div className="icon text-5xl bg-sky-900 text-white rounded-full p-2 group-hover:bg-sky-600 ">
+                            <CiLocationOn />
                         </div>
+                        <div className="content ml-3">
+                            <h5 className='text-gray-700 text-xl font-medium'>Our Address</h5>
+                            <p className='text-gray-600'>1234 , Puttalam -Colombo Rd, Negombo, Sri Lanka.</p>
+                        </div>
+                    </div>
+
+                    <div className="contact-item flex items-center bg-white rounded-lg p-4 mb-5 border border-gray-300 group transition-all">
+                        <div className="icon text-5xl bg-sky-900 text-white rounded-full p-2 group-hover:bg-sky-600">
+                            <CiPhone />
+                        </div>
+                        <div className="content ml-3">
+                            <h5 className='text-gray-700 text-xl font-medium'>Phone Number</h5>
+                            <p className='text-gray-600'>+94 (033) 456-7890</p>
+                            <p className='text-gray-600'>+94 (033) 456-7892</p>
+                        </div>
+                    </div>
+
+                    <div className="contact-item flex items-center bg-white rounded-lg p-4 mb-5 border border-gray-300 group transition-all">
+                        <div className="icon text-5xl bg-sky-900 text-white rounded-full p-2 group-hover:bg-sky-600">
+                            <CiMail />
+                        </div>
+                        <div className="content ml-3">
+                            <h5 className='text-gray-700 text-xl font-medium'>Email Address</h5>
+                            <p className='text-gray-600'>info@leonine.com</p>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+
+
+            <section className="w-full">
+                <div className="w-[1200px] flex mx-auto ">
+                    <div className="grid grid-cols-2 gap-4">
+                       
 
                         <div className='p-4'>
 
@@ -71,19 +80,18 @@ function ContactUs() {
                     </div>
 
                 </div>
-
             </section>
 
             <section className='map'>
-                <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12580.78211163128!2d145.053135!3d-37.972566!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad646b5d2ba4df7%3A0x4045675218ccd90!2sMelbourne%20VIC%2C%20Australia!5e0!3m2!1sen!2sus!4v1731678356375!5m2!1sen!2sus"
-                width="100%" 
-                height="450" 
-                style={{border:0, display:"block"}} allowFullScreen="" 
-                loading="lazy"
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12580.78211163128!2d145.053135!3d-37.972566!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad646b5d2ba4df7%3A0x4045675218ccd90!2sMelbourne%20VIC%2C%20Australia!5e0!3m2!1sen!2sus!4v1731678356375!5m2!1sen!2sus"
+                    width="100%"
+                    height="450"
+                    style={{ border: 0, display: "block" }} allowFullScreen=""
+                    loading="lazy"
                 ></iframe>
             </section>
-            
+
         </>
     )
 }
