ece88bdef5a6180c705d33b7a77a2ee5faf01795
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-23 18:31:52 +0530

    set user data for sending

--- src/pages/register-page/RegisterPage.jsx
+++ src/pages/register-page/RegisterPage.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react"
 import axios from "axios"
-import { CiHome, CiLock, CiMail } from "react-icons/ci"
+import { CiHome, CiLock, CiMail, CiUser } from "react-icons/ci"
 import { Link } from "react-router-dom"
 
 export default function RegisterPage() {
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+    const initialUser = { firstName:"", lastName: "", email:"", password:"", confirmPassword:"" }
+    const [user, setUser] = useState(initialUser);
 
-    function handleRegistration(){
+
+    function handleChange(e){
+        const {name, value} = e.target
+        setUser((prevData) =>({
+            ...prevData, 
+            [name]: value 
+        }))
+    }
+
+    function handleRegistration() {
 
     }
 
@@ -17,23 +26,48 @@ export default function RegisterPage() {
             <div className="w-full h-screen flex justify-center items-center bg-cover bg-center " style={{ backgroundImage: `url('./images/wrappers/login.jpg')` }}>
 
                 <div className="login-card max-w-[350px] lg:max-w-[700px] bg-gray-50 flex flex-col lg:flex-row w-full rounded-lg overflow-hidden shadow-lg">
-                    <div className="login-image lg:w-1/2 bg-gray-200 bg-cover bg-center" style={{ backgroundImage: `url('./images/pages/login/side-image.jpg')` }}></div>
+                    <div className="login-image lg:w-1/2 bg-gray-200 bg-cover bg-center" style={{ backgroundImage: `url('./images/pages/register/register.jpg')` }}></div>
                     <div className="login-form lg:w-1/2 p-6">
                         <h2 className="text-3xl mt-5 mb-2 text-gray-500" >Register now</h2>
                         <p className="text-sm text-gray-400 mb-10">Register with your personal information</p>
 
                         <form action="">
+
+                            <div className="input-group flex items-center border-b border-gray-300 bg-gray-200 py-2 px-3 focus-within:bg-gray-50">
+                                <div className="icon text-3xl text-gray-500">
+                                    <CiUser />
+                                </div>
+                                <div className="input w-full flex flex-col px-2">
+                                    <label htmlFor="" className="text-xs text-gray-400">First name</label>
+                                    <input type="text" name="firstName" placeholder="John" className="w-full text-sm outline-none bg-transparent" required value={user.firstName} onChange={handleChange} />
+                                </div>
+                                <div className="verified">
+
+                                </div>
+
+                            </div>
+
+                            <div className="input-group flex items-center border-b border-gray-300 bg-gray-200 py-2 px-3 focus-within:bg-gray-50">
+                                <div className="icon text-3xl text-gray-500">
+                                    <CiUser />
+                                </div>
+                                <div className="input w-full flex flex-col px-2">
+                                    <label htmlFor="" className="text-xs text-gray-400">Last name</label>
+                                    <input type="text" name="lastName" placeholder="Doe" className="w-full text-sm outline-none bg-transparent" required value={user.lastName} onChange={handleChange} />
+                                </div>
+                                <div className="verified">
+
+                                </div>
+
+                            </div>
+
                             <div className="input-group flex items-center border-b border-gray-300 bg-gray-200 py-2 px-3 focus-within:bg-gray-50">
                                 <div className="icon text-3xl text-gray-500">
                                     <CiMail />
                                 </div>
-                                <div className="input flex flex-col px-2">
+                                <div className="input w-full flex flex-col px-2">
                                     <label htmlFor="" className="text-xs text-gray-400">Email Address</label>
-                                    <input type="email" name="email" placeholder="yourmail@example.com" className="w-full text-sm outline-none bg-transparent" required defaultValue={email} onChange={
-                                        (e) => {
-                                            setEmail(e.target.value)
-                                        }
-                                    } />
+                                    <input type="email" name="email" placeholder="yourmail@example.com" className="w-full text-sm outline-none bg-transparent" required value={user.email} onChange={handleChange} />
                                 </div>
                                 <div className="verified">
 
@@ -45,13 +79,23 @@ export default function RegisterPage() {
                                 <div className="icon text-3xl text-gray-500">
                                     <CiLock />
                                 </div>
-                                <div className="input flex flex-col px-2">
+                                <div className="input w-full flex flex-col px-2">
                                     <label htmlFor="" className="text-xs text-gray-400">Password</label>
-                                    <input type="password" name="password" placeholder="*********" className="w-full text-sm outline-none bg-transparent" required defaultValue={password} onChange={
-                                        (e) => {
-                                            setPassword(e.target.value)
-                                        }
-                                    } />
+                                    <input type="password" name="password" placeholder="*********" className="w-full text-sm outline-none bg-transparent" required value={user.password} onChange={handleChange} />
+                                </div>
+                                <div className="verified">
+
+                                </div>
+
+                            </div>
+
+                            <div className="input-group flex items-center border-b border-gray-300 bg-gray-200 py-2 px-3 focus-within:bg-gray-50">
+                                <div className="icon text-3xl text-gray-500">
+                                    <CiLock />
+                                </div>
+                                <div className="input w-full flex flex-col px-2">
+                                    <label htmlFor="" className="text-xs text-gray-400">Password Confirmation</label>
+                                    <input type="password" name="confirmPassword" placeholder="*********" className="w-full text-sm outline-none bg-transparent" required value={user.confirmPassword} onChange={handleChange} />
                                 </div>
                                 <div className="verified">
 
@@ -60,7 +104,7 @@ export default function RegisterPage() {
                             </div>
 
 
-                          
+
 
 
                             <div className="buttons flex mt-10 mb-5">
