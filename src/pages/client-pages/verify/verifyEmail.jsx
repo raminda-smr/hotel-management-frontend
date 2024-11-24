ed21060c159172cdfa4349125349a3da647f4fa9
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-24 18:25:05 +0530

    create basic template

--- src/pages/client-pages/verify/verifyEmail.jsx
+++ src/pages/client-pages/verify/verifyEmail.jsx
@@ -1,7 +1,13 @@
 export default function VerifyEmail() {
-  return (
-    <div className="p-20">
-        
-    </div>
-  )
+
+
+    return (
+        <div className="p-20 flex justify-center">
+            <div className="loading-box max-w-[350px] flex flex-col text-center">
+                <h2 className="text-5xl mb-4 text-gray-400">Please Wait</h2>
+                <img src="/images/animated/infinite-spinner.svg" alt="" className="w-32 mx-auto" />
+                <h4 className="text-gray-500 mt-4">Verifing your email...</h4>
+            </div>
+        </div>
+    )
 }
