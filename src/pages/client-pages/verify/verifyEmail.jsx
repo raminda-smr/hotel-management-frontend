cae54734b71075d4355ec3aa3b0da89332fafaca
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-25 07:35:35 +0530

    show success message

--- src/pages/client-pages/verify/verifyEmail.jsx
+++ src/pages/client-pages/verify/verifyEmail.jsx
@@ -1,7 +1,8 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import toast from "react-hot-toast"
-import { CiHome, CiMail } from "react-icons/ci"
+import { CiHome, CiLogin, CiMail } from "react-icons/ci"
+import { FaRegCheckCircle } from "react-icons/fa"
 import { Link, useNavigate, useParams } from "react-router-dom"
 
 export default function VerifyEmail() {
@@ -44,10 +45,10 @@ export default function VerifyEmail() {
     }, [])
 
 
-    function handleVerificationRequest(e){
+    function handleVerificationRequest(e) {
         e.preventDefault()
 
-        if(isLoading){
+        if (isLoading) {
             return
         }
 
@@ -59,17 +60,17 @@ export default function VerifyEmail() {
             (result) => {
                 if (result) {
                     // redirect to login
-                    toast.success("Your verification mail sent. Please verify within an hour.",{duration:20000})
+                    toast.success("Your verification mail sent. Please verify within an hour.", { duration: 20000 })
                     navigate('/login')
                 }
             }
         ).catch(
             (error) => {
                 if (error) {
-                    toast.error("Your verification mail sending failed. " + error.message )
+                    toast.error("Your verification mail sending failed. " + error.message)
                     setIsLoading(false)
                 }
-               
+
             }
         )
 
@@ -96,7 +97,7 @@ export default function VerifyEmail() {
                         <h2 className="text-5xl mb-4 text-gray-400  text-center">Verification Expired</h2>
 
                         <p className="text-gray-500 mb-4">Request a new verification link</p>
-                        <form action="" onSubmit={(e)=>{ handleVerificationRequest(e) }}>
+                        <form action="" onSubmit={(e) => { handleVerificationRequest(e) }}>
                             <div className="input-group flex items-center border-b border-gray-300 bg-gray-200 py-2 px-3 focus-within:bg-gray-50">
                                 <div className="icon text-3xl text-gray-500">
                                     <CiMail />
@@ -119,12 +120,12 @@ export default function VerifyEmail() {
                             <div className="buttons flex justify-end mt-3 mb-5">
                                 <button type="submit" className="bg-sky-600 font-medium text-white px-5 py-2 rounded-full shadow-md  text-sm hover:bg-sky-700" >
                                     {
-                                        isLoading ? 
-                                        (<div className="border-2 border-t-white w-[20px] h-[20px] rounded-full animate-spin"></div>)
-                                        : 
-                                        (<span>Request a new verification link</span>)
+                                        isLoading ?
+                                            (<div className="border-2 border-t-white w-[20px] h-[20px] rounded-full animate-spin"></div>)
+                                            :
+                                            (<span>Request a new verification link</span>)
                                     }
-                                    
+
                                 </button>
                             </div>
 
@@ -136,6 +137,23 @@ export default function VerifyEmail() {
                             </div>
 
                         </form>
+                    </div>
+                )
+            }
+
+
+            {
+                // show if verification not completed
+                isVerificationChecked && !isExpired && isVerified && (
+                    <div className="loading-box max-w-[700px] flex flex-col text-center">
+                        <h2 className="text-5xl mb-4 text-gray-400">Account verified!</h2>
+                        <FaRegCheckCircle className="text-green-600 text-8xl mx-auto" />
+                        <h4 className="text-gray-500 mt-4 mb-4">Your email verified.</h4>
+
+                        <Link to="/login" className="flex bg-blue-500 items-center justify-center py-2 text-white rounded-lg transition-all hover:bg-blue-600">
+                            <CiLogin className="text-2xl mt-1 mr-1" />
+                            <span>Login with your credentials</span>
+                        </Link>
                     </div>
                 )
             }
