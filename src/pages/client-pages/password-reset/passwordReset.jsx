d5da08909b6bd8350c10ff6bc2929d4a825083e6
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-26 22:14:39 +0530

    create password reset function

--- src/pages/client-pages/password-reset/passwordReset.jsx
+++ src/pages/client-pages/password-reset/passwordReset.jsx
@@ -1,19 +1,53 @@
+import axios from "axios";
+import { useState } from "react";
+import { CiHome, CiMail } from "react-icons/ci";
+import { Link } from "react-router-dom";
+
 export default function PasswordReset() {
 
-    const [email, setEmail] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
+    const [email, setEmail] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
 
     function handlePasswordResetRequest(e) {
-       
+        e.preventDefault()
+        if (isLoading) {
+            return
+        }
 
+        setIsLoading(true)
+
+        axios.post(import.meta.env.VITE_BACKEND_URL + '/api/users/request-password-reset', {
+            email: email
+        }).then(
+            (result) => {
+                if (result) {
+                    // redirect to login
+                    toast.success("Your password reset mail sent. Please reset within day.", { duration: 20000 })
+                    navigate('/login')
+                }
+                else{
+                    setIsLoading(false)
+                }
+            }
+        ).catch(
+            (error) => {
+                if (error) {
+                    toast.error("Your password reset mail sending failed. " + error.message)
+                   
+                }
+                
+                setIsLoading(false)
+            }
+        )
     }
 
     return (
-        <div className="py-20">
+        <div className="py-20  flex justify-center">
             <div className="loading-box max-w-[350px] flex flex-col">
-                <h2 className="text-5xl mb-4 text-gray-400  text-center">Verification Expired</h2>
+                <h2 className="text-3xl mb-4 text-gray-400  text-center">Forgot your password?</h2>
 
-                <p className="text-gray-500 mb-4">Request a new verification link</p>
+                <p className="text-gray-500 mb-2 text-center font-semibold">Request a password-reset link</p>
+                <p className="text-gray-500 text-sm mb-4 text-center">The link will be sent to your email address. Please enter the account created email address below.</p>
                 <form action="" onSubmit={(e) => { handlePasswordResetRequest(e) }}>
                     <div className="input-group flex items-center border-b border-gray-300 bg-gray-200 py-2 px-3 focus-within:bg-gray-50">
                         <div className="icon text-3xl text-gray-500">
@@ -40,7 +74,7 @@ export default function PasswordReset() {
                                 isLoading ?
                                     (<div className="border-2 border-t-white w-[20px] h-[20px] rounded-full animate-spin"></div>)
                                     :
-                                    (<span>Request a new verification link</span>)
+                                    (<span>Request a new link</span>)
                             }
 
                         </button>
