846876438423de126fc961f7c8fcaa2430b704ea
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-27 23:16:46 +0530

    create user get function

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AdminPage from './pages/admin-pages/adminPage'
 import HomePage from './pages/client-pages/homePage'
 import LoginPage from './pages/login-page/LoginPage'
 import { Toaster } from 'react-hot-toast'
 import RegisterPage from './pages/register-page/RegisterPage'
+import UserContext from './context/userContext'
 
 
 function App() {
-    const [count, setCount] = useState(0)
+
+    const { user, setUser } = useContext(UserContext)
+    const [userLogged, setUserLogged] = useState(false)
+    
+    useEffect(()=>{
+        
+        const token = localStorage.getItem('token')
+
+        if(token != null){
+            axios.get(import.meta.env.VITE_BACKEND_URL + '/api/users/logged',{
+                headers:{
+                    "Authorization" : 'Bearer ' + token,
+                    "Content-Type" : "application/json"
+                }
+            })
+            .then(
+                (res)=>{
+                    setUser(res.data.user)
+                    setUserLogged(true)
+                }
+            ).catch(
+                (err)=>{
+                    setUserLogged(false)
+                }
+            )
+        }
+        else{
+            setUserLogged(false)
+        }
+    },[userLogged])
+
 
     return (
         <BrowserRouter>
