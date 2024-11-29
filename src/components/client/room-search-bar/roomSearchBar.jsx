a46f0d018f4cc2fa8340c409db6eac1d93092dde
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-30 03:43:28 +0530

    function home searchbar

--- src/components/client/room-search-bar/roomSearchBar.jsx
+++ src/components/client/room-search-bar/roomSearchBar.jsx
@@ -1,21 +1,45 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
 import { CiCalendar, CiCalendarDate, CiGrid41 } from "react-icons/ci"
+import { useNavigate } from "react-router-dom";
 
 
 export default function RoomSearchBar() {
+    
+    const navigate = useNavigate()
 
-    const [categories, setCategories] = useState([]);
+    const initialSearchData = {start:"", end:"", category:"" }
+    const [searchData, setSearchData] = useState(initialSearchData)
+    const [categories, setCategories] = useState([])
+
     useEffect(() => {
         axios.get(import.meta.env.VITE_BACKEND_URL + '/api/categories', {}).then(
             (result) => {
                 setCategories(result.data.list)
             }
         )
+       
     }, [categories])
 
+    function handleChange(e){
+        const { name, value } = e.target; 
+        const updatedSearchData = { ...searchData, [name]: value }; 
+        setSearchData(updatedSearchData); 
+       
+    };
+
+    function handleSearch(e){
+        e.preventDefault()
+        
+        if(searchData.start != "" &&  searchData.end != "" && searchData.category != ""){
+            localStorage.setItem("searchData", JSON.stringify(searchData)); // Save to localStorage
+            navigate("/room-search");
+        }
+    }
+
     return (
-        <form className='bg-blue-500 flex flex-col lg:flex-row rounded-lg justify-center items-center py-2 px-1   min-w-[260px]'>
+        <>
+        <form className='bg-blue-500 flex flex-col lg:flex-row rounded-lg justify-center items-center py-2 px-1 min-w-[260px]'  onSubmit={(e) => {handleSearch(e)}}  >
             <div className="flex flex-col w-full md:flex-row">
 
                 <div className="input-group flex items-center border-b border-gray-300 bg-white py-2 px-3 mb-2 mx-1 focus-within:bg-gray-50">
@@ -24,7 +48,7 @@ export default function RoomSearchBar() {
                     </div>
                     <div className="input w-full flex flex-col px-2">
                         <label htmlFor="" className="text-xs text-gray-400">Check-in Date</label>
-                        <input type="date" name="start" className="w-full text-sm outline-none bg-transparent" required />
+                        <input type="date" name="start" className="w-full text-sm outline-none bg-transparent" required value={searchData.start} onChange={handleChange} />
                     </div>
                 </div>
 
@@ -34,7 +58,7 @@ export default function RoomSearchBar() {
                     </div>
                     <div className="input w-full flex flex-col px-2">
                         <label htmlFor="" className="text-xs text-gray-400">Check-out Date</label>
-                        <input type="date" name="end" className="w-full text-sm outline-none bg-transparent" required />
+                        <input type="date" name="end" className="w-full text-sm outline-none bg-transparent" required value={searchData.end} onChange={handleChange}  />
                     </div>
                 </div>
 
@@ -47,7 +71,7 @@ export default function RoomSearchBar() {
                     </div>
                     <div className="input w-full flex flex-col px-2">
                         <label htmlFor="" className="text-xs text-gray-400">Category</label>
-                        <select className="outline-none bg-transparent" name="category" id="">
+                        <select className="outline-none bg-transparent" name="category" id="" value={searchData.category} onChange={handleChange} >
                             {categories.map((category, index) => (
                                 <option key={index} value={category.name}>
                                     {category.name}
@@ -60,9 +84,10 @@ export default function RoomSearchBar() {
             </div>
             <div className="flex w-full">
 
-                <button className="bg-amber-400 px-6 py-4 w-full min-w-[200px] lg:mb-2 lg:rounded-none mx-1 font-medium uppercase  rounded-lg transition-all duration-300 overflow-hidden hover:bg-amber-500 hover:text-white ">Book Now</button>
+                <button type="submit" className="bg-amber-400 px-6 py-4 w-full min-w-[200px] lg:mb-2 lg:rounded-none mx-1 font-medium uppercase  rounded-lg transition-all duration-300 overflow-hidden hover:bg-amber-500 hover:text-white ">Book Now</button>
 
             </div>
         </form>
+        </>
     )
 }
