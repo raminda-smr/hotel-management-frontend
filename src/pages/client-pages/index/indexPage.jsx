0c2e41e5a5cd6f728a5e4a1909dc65aaa1d7cfaf
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-12-02 08:15:46 +0530

    load gallery images

--- src/pages/client-pages/index/indexPage.jsx
+++ src/pages/client-pages/index/indexPage.jsx
@@ -10,6 +10,7 @@ import RoomCategory from "../../../components/client/room-category/roomCategory"
 export default function IndexPage() {
 
     const [roomCategories, setRoomCategories] = useState([])
+    const [galleryItems, setGalleryItems] = useState([])
 
     const slides = [
         { "url": "/slides/slide-1.jpg", "title": "Welcome Lionion Vila Resort", "subtitle": "Beach Resort" },
@@ -30,12 +31,12 @@ export default function IndexPage() {
     }
 
 
-    const galleryItems = [
-        { "image": "https://amorgoshotel.com/wp-content/uploads/2014/12/Amorgos-Standard-Room1-e1464286427430.jpg", "name": "Gallery Item", "description": "Gallery description" },
-        { "image": "https://amorgoshotel.com/wp-content/uploads/2014/12/Amorgos-Standard-Room1-e1464286427430.jpg", "name": "Gallery Item", "description": "Gallery description" },
-        { "image": "https://amorgoshotel.com/wp-content/uploads/2014/12/Amorgos-Standard-Room1-e1464286427430.jpg", "name": "Gallery Item", "description": "Gallery description" },
-        { "image": "https://amorgoshotel.com/wp-content/uploads/2014/12/Amorgos-Standard-Room1-e1464286427430.jpg", "name": "Gallery Item", "description": "Gallery description" }
-    ]
+    // const galleryItems = [
+    //     { "image": "https://amorgoshotel.com/wp-content/uploads/2014/12/Amorgos-Standard-Room1-e1464286427430.jpg", "name": "Gallery Item", "description": "Gallery description" },
+    //     { "image": "https://amorgoshotel.com/wp-content/uploads/2014/12/Amorgos-Standard-Room1-e1464286427430.jpg", "name": "Gallery Item", "description": "Gallery description" },
+    //     { "image": "https://amorgoshotel.com/wp-content/uploads/2014/12/Amorgos-Standard-Room1-e1464286427430.jpg", "name": "Gallery Item", "description": "Gallery description" },
+    //     { "image": "https://amorgoshotel.com/wp-content/uploads/2014/12/Amorgos-Standard-Room1-e1464286427430.jpg", "name": "Gallery Item", "description": "Gallery description" }
+    // ]
 
     const feedbacks = [
         { email: "johndoe@example.com", username: "JohnDoe", title: "Great Service!", description: "I had a wonderful experience with the customer service team. They were very helpful and resolved my issue quickly.", date: "2023-11-01T10:00:00Z", approved: true },
@@ -47,14 +48,24 @@ export default function IndexPage() {
     // get room categories 
     useEffect(() => {
         axios.get(import.meta.env.VITE_BACKEND_URL + '/api/categories', {}).then(
-
             (res) => {
                 if (res) {
                     // console.log(res)
                     setRoomCategories(res.data.list)
                 }
             }
+        )
+    }, [])
 
+    // get gallery items  
+    useEffect(() => {
+        axios.get(import.meta.env.VITE_BACKEND_URL + '/api/gallery?limit=8', {}).then(
+            (res) => {
+                if (res) {
+                    // console.log(res)
+                    setGalleryItems(res.data.list)
+                }
+            }
         )
     }, [])
 
@@ -78,7 +89,7 @@ export default function IndexPage() {
             </section>
 
 
-            { roomCategories && roomCategories.length > 0 && (
+            {roomCategories && roomCategories.length > 0 && (
                 <section className="our-rooms py-20 bg-blue-500">
                     <div className="max-w-[768px] mx-auto px-4 mb-8">
                         <h2 className="text-white text-5xl text-center mb-3">Our Rooms</h2>
@@ -87,7 +98,7 @@ export default function IndexPage() {
 
                     <div className="max-w-[1200px] mx-auto">
                         <div className="room-categories grid gap-4 px-4 lg:grid-cols-3">
-                            {roomCategories.map((category, index)=>(
+                            {roomCategories.map((category, index) => (
                                 <RoomCategory key={index} category={category} />
                             ))}
                         </div>
@@ -97,21 +108,29 @@ export default function IndexPage() {
 
 
 
-            <section className="our-gallery py-20 bg-gray-100">
+            {galleryItems && galleryItems.length > 0 && (
+                <section className="our-gallery py-20 bg-gray-100">
 
-                <div className="max-w-[768px] mx-auto px-4 mb-8">
-                    <h2 className="text-gray-500 text-5xl text-center mb-3">Gallery</h2>
-                    <p className="text-center text-gray-500 text-xl font-thin ">Capture the Beauty of Every Moment – Explore Leonine Villa's Stunning Spaces</p>
-                </div>
-
-                <div className="max-w-[1200px] mx-auto">
-                    <div className="gallery-items grid gap-4 grid-cols-2 px-4 lg:grid-cols-4">
-
-                        {galleryItems.map((item, index) => <GalleryItem key={index} item={item} />)}
-
+                    <div className="max-w-[768px] mx-auto px-4 mb-8">
+                        <h2 className="text-gray-500 text-5xl text-center mb-3">Gallery</h2>
+                        <p className="text-center text-gray-500 text-xl font-thin ">Capture the Beauty of Every Moment – Explore Leonine Villa's Stunning Spaces</p>
                     </div>
-                </div>
-            </section>
+
+                    <div className="max-w-[1200px] mx-auto">
+                        <div className="gallery-items grid gap-4 grid-cols-2 px-4 lg:grid-cols-4">
+
+                            {galleryItems.map((item, index) => <GalleryItem key={index} item={item} />)}
+
+                        </div>
+                    </div>
+
+                    <div className="max-w-[1200px] mx-auto text-center mt-4">
+                        <Link className='text-white bg-blue-600 px-5 py-2 rounded-full inline-block mx-auto mt-3 mb-4 transition-all hover:bg-amber-500' to="/gallery"> View more</Link>
+                    </div>
+                </section>
+            )}
+
+
 
 
             <section className="our-feedbacks py-20 bg-gray-300">
